4f5a76314abe5a62cf9d3f29b5ccf33d92d7ca74
Narendra-Git1 <narendray353@gmail.com>
2026-05-21 15:33:02 +0530

    Initialized React frontend with Tailwind CSS and routing setup

---
+++ techshop-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+
+    <div className="bg-black text-white h-screen flex items-center justify-center text-4xl">
+
+      Welcome to TechShop Frontend
+
+    </div>
+  )
+}
+
+export default App
